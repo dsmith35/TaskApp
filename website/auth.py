9d3c3512f7e24d77b4555946940c17c319329191
artyhu <122575135+artyhu@users.noreply.github.com>
2023-10-20 22:12:22 -0400

    more elements of front end added

--- website/auth.py
+++ website/auth.py
@@ -5,11 +5,28 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/register')
 def register():
-    return render_template("login.html", user=current_user)
+    return render_template("register.html", user=current_user)
 
 @auth.route('/login')
 def login():
-    return render_template("register.html", user=current_user)
+    return render_template("login.html", user=current_user)
+
+@auth.route('/taskmanager')
+def taskManager():
+    return render_template("taskManager.html", user=current_user)
+
+@auth.route('/projectmanager')
+def projectManager():
+    return render_template("projectManager.html", user=current_user)
+
+@auth.route('/newtask')
+def newTask():
+    return render_template("newTask.html", user=current_user)
+
+@auth.route('/newproject')
+def newProject():
+    return render_template("newProject.html", user=current_user)
+
 
 @auth.route('/logout')
 def logout():
