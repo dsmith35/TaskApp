f8da3fc95732a2de1094e56bbf1e5bf087babd69
artyhu <122575135+artyhu@users.noreply.github.com>
2023-11-03 18:28:55 -0400

    Database implementation partway

--- website/auth.py
+++ website/auth.py
@@ -1,13 +1,34 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import current_user
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/register')
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        username = request.form.get('username')
+        name = request.form.get('name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 3:
+            flash('Email must contain more than three characters', category ='error')
+        elif len(name) < 2:
+            flash('Name must contain more than two characters', category ='error')
+        elif password != password2:
+            flash('Passwords do not match', category ='error')
+        elif len(password) < 6:
+            flash('Password must contain more than six characters', category ='error')
+
+        else:
+            flash('Success! Your account has been created', category='success')
+            #add user to db
+            
+            
     return render_template("register.html", user=current_user)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html", user=current_user)
 
---
+++ website/models.py
@@ -0,0 +1,24 @@
+from . import db 
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), unique = True)
+    name = db.Column(db.String(50))
+    password = db.Column(db.String(50))
+    
+    
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    info = db.Column(db.String(9999))
+    date = db.column(db.DateTime(timezone=True), default=func.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    info = db.Column(db.String(9999))
+    date = db.column(db.DateTime(timezone=True), default=func.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    task_id = db.Column(db.String, db.ForeignKey('task.id'))
