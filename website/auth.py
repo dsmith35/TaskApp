8ee86d7fa185beb954e5dacd10968f255a811260
Zack Watters <117243384+ZJWatters@users.noreply.github.com>
2023-11-13 17:32:08 -0500

    tasks canbe assigned to projects now

--- website/auth.py
+++ website/auth.py
@@ -85,33 +85,32 @@ def new_task():
             flash('Task name is required', category='error')
         elif not start_date:
             flash('Start date is required', category='error')
-        elif start_date >= deadline:
+        elif deadline and start_date >= deadline:
             flash('Start date must be before the deadline', category='error')
         else:
-            # Convert date strings to datetime objects (SQLAlchemy will only accept datetime objects)
+            # Convert date strings to datetime objects
             start_date = datetime.strptime(start_date, '%Y-%m-%d')
-            deadline = datetime.strptime(deadline, '%Y-%m-%d') if deadline else None
+            deadline = datetime.strptime(deadline, '%Y-%m-%d') if deadline else datetime.strptime('9999-12-31','%Y-%m-%d')
 
             # Check if project_id is provided
             if project_id:
                 # If project_id is provided, associate the task with the specified project
-                project = Project.query.get(project_id)
-                if project in project.users:
+                project = Project.query.get(int(project_id))
+                if project and current_user in project.users:
                     new_task = Task(
                         name=name,
                         assignee=assignee,
                         description=description,
                         start_date=start_date,
                         deadline=deadline,
-                        project_id=project_id,
-                        user_id=current_user.id
+                        project_id=int(project_id)
                     )
                     db.session.add(new_task)
                     db.session.commit()
                     flash(f'New task "{name}" assigned to project "{project.name}" successfully', category='success')
-                    return redirect(url_for('auth.task_manager', project_id=project_id))
+                    return redirect(url_for('auth.project', project_id=project_id))
                 else:
-                    flash('Invalid project selection', category='error')
+                    flash('Invalid project selection or access denied', category='error')
             else:
                 # If project_id is not provided, create a standalone task without associating it with any project
                 new_task = Task(
@@ -119,8 +118,7 @@ def new_task():
                     assignee=assignee,
                     description=description,
                     start_date=start_date,
-                    deadline=deadline,
-                    user_id=current_user.id
+                    deadline=deadline
                 )
                 db.session.add(new_task)
                 db.session.commit()
@@ -130,6 +128,8 @@ def new_task():
     # Provide the project_id to the template for creating the form
     project_id = request.args.get('project_id')
     return render_template("newTask.html", user=current_user, project_id=project_id)
+
+
 
 @auth.route('/newproject', methods=['GET', 'POST'])
 def new_project():
