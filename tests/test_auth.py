63396c61420e205976e8ec6489bc4b805de7853b
artyhu <122575135+artyhu@users.noreply.github.com>
2023-11-13 23:40:24 -0500

    Multiple cases/test case

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -5,23 +5,33 @@ import datetime
 
 # start_date = datetime.datetime.strptime("2023-11-20 01-01-01", "%Y-%m-%d %H-%m-%s")
 # deadline = datetime.datetime.strptime("2023-12-20 01-01-01", "%Y-%m-%d %H-%m-%s")
-data1 = {"email":"test@test.com","name":"testguy","password1":"123456","password2":"123456"}
+
 bdata = {"name": "Johnn", "sdate": "2023-11-20", "deadline": "2023-12-20", "description": "327FinalAsssignment" }
 
+@pytest.mark.parametrize("name, sdate, deadline, description",[
+    ("Johnn", "2023-11-20", "2023-12-20", "327FinalAsssignment" ),
+    ("Johnn", "2023-12-20", "2023-11-20", "327FinalAsssignment" ),
+    ("Johnn", "", "2023-11-20", "327FinalAsssignment" ),
+    ("Johnn", "2023-12-20", "", "327FinalAsssignment" )
+    
+    
+])
+#def test_newproject(client, app):
+#    response = client.post("/newproject", data = bdata)
+#    with app.app_context():
+#        assert bdata["name"] == "Johnn"
+#        assert bdata["description"] == "327FinalAsssignment"
+#        assert bdata["sdate"] < bdata["deadline"]
+#        assert bdata["sdate"] != ""
+#        assert bdata["deadline"] != ""      
+def test_newproject(name, sdate, deadline, description):
+    assert name == "Johnn"
+    assert description == "327FinalAsssignment"
+    assert sdate < deadline
+    assert sdate != ""
+    assert deadline != ""
 
-
-def test_register(client, app):
-    response = client.post("/register", data=data1)
-    with app.app_context():
-        assert User.query.count() == 1
-        assert User.query.first().email == "test@test.com"
-        
-
-def test_newproject(client, app):
-    response = client.post("/newproject", data = bdata)
-    with app.app_context():
-        assert bdata["name"] == "Johnn"
-        assert bdata["description"] == "327FinalAsssignment"
-        assert bdata["sdate"] < bdata["deadline"]
-        assert bdata["sdate"] != ""
-        assert bdata["deadline"] != ""      
+#def test_login(client, app):
+ #   response = client.post("/login", data = cdata)
+  #  with app.app_context():
+   #     assert 
