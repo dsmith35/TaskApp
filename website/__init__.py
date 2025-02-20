b93ecfc42774526c409efb128cb2c84ee0689c10
Dylan Smith <dylansmith9001@gmail.com>
2025-02-20 14:26:38 -0500

    u

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+from os import path, getenv
+import os
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -14,6 +16,18 @@ def create_app(testing=False):
     if testing: 
         app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{TEST_DB_NAME}'
     db.init_app(app)
+
+    is_vercel = getenv('VERCEL') == '1'
+
+    # Set database path
+    if is_vercel:
+        # Use writable /tmp directory on Vercel
+        db_path = os.path.join('/tmp', DB_NAME if not testing else TEST_DB_NAME)
+    else:
+        # Local development: place DB in instance folder
+        db_folder = path.join(app.instance_path)
+        os.makedirs(db_folder, exist_ok=True)
+        db_path = path.join(db_folder, DB_NAME if not testing else TEST_DB_NAME)
 
     
     from .views import views
