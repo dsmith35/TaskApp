56c281f1ff59b42a4cac4a1d1c846abcf5328bb2
Dylan Smith <dylansmith9001@gmail.com>
2025-02-20 14:29:44 -0500

    u

--- website/__init__.py
+++ website/__init__.py
@@ -1,21 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
 from flask_login import LoginManager
 from os import path, getenv
 import os
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
-TEST_DB_NAME="test-database.db"
+TEST_DB_NAME = "test-database.db"
+
 # initialize db
 def create_app(testing=False):
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'abcd'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    if testing: 
-        app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{TEST_DB_NAME}'
-    db.init_app(app)
 
     is_vercel = getenv('VERCEL') == '1'
 
@@ -29,27 +25,27 @@ def create_app(testing=False):
         os.makedirs(db_folder, exist_ok=True)
         db_path = path.join(db_folder, DB_NAME if not testing else TEST_DB_NAME)
 
-    
+    # Set the correct DB URI
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+    db.init_app(app)
+
     from .views import views
     from .auth import auth
-    
+
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
-    
+
     from .models import User, Task, Project
-        
+
     with app.app_context():
         db.create_all()
-
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
-    
+
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
-    
+
     return app
-
-
